709b074b520ace6eef6d14f9ce2ab182ef21c5ce
Ya <nesquik1723@gmail.com>
2026-03-02 13:04:39 +0300

    recipe-generator

---
+++ venv/auth.py
@@ -0,0 +1,58 @@
+import os
+from datetime import datetime, timedelta, timezone
+from dotenv import load_dotenv
+from jose import JWTError, jwt
+from passlib.context import CryptContext
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+from models import UserInDB
+from database import users_db
+
+load_dotenv()
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = os.getenv("ALGORITHM")
+ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", "30"))
+
+pwd_context = CryptContext(schemes=['bcrypt'],deprecated='auto')
+
+# Получаем пароль и хеширум его
+def hash_password(password:str) -> str:
+    return pwd_context.hash(password)
+
+# Для сравнения введённого и захешированной версии  
+def verify_pass(user_password:str, hashed_user_password: str)-> bool:
+    return pwd_context.verify(user_password,hashed_user_password)
+
+# Создание аццесс токена
+def create_access_token(data:dict, expires_delta: timedelta | None = None)-> str:
+    to_encode=data.copy()
+    if expires_delta:
+        expire=datetime.now(timezone.utc) + expires_delta
+    else:
+        expire=datetime.now(timezone.utc)+timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({'exp':expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+
+# шаблон из Fastapi, реализует способ аунтификации по протоколу oauth2
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl='login')
+
+async def get_current_user(token:str = Depends(oauth2_scheme))->UserInDB:
+    credentials_exception=HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="cloud not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"}
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get('sub')
+        if username is None:
+            raise credentials_exception
+    except JWTError:
+        raise credentials_exception
+    
+    user=users_db.get(username)
+    if user is None:
+        raise credentials_exception
+    return UserInDB(**user)
