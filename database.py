64759b6e375033503f73c48eea38497eddd74fa5
Ya <nesquik1723@gmail.com>
2026-03-03 13:31:13 +0300

    models

--- database.py
+++ database.py
@@ -1,1 +1,17 @@
 users_db={}
+recipes_db={}
+
+from sqlmodel import SQLModel, create_engine, Session
+
+sqlite_url = "sqllite///./database.db"
+
+
+engine = create_engine(sqlite_url, echo=True)
+
+def create_db_tables():
+ SQLModel.metadata.create_all(engine)
+
+# ................
+def get_session():
+ with Session(engine) as session:
+  yield session
--- models.py
+++ models.py
@@ -1,4 +1,29 @@
 from pydantic import BaseModel
+from sqlmodel import SQLModel, Field, Relationship
+from typing import Optional, List
+from datetime import datetime, timezone
+
+class UserDB(SQLModel, table=True):
+    __tablename__ = "users"
+
+    id: Optional[int] = Field(default=None,primary_key=True) # первичный ключ
+    username: str = Field(unique=True, index=True)
+    hased_password: str
+    recipes: List["RecipeDB"]=Relationship(back_populates='user')
+
+class RecipeDB(SQLModel,table=True):
+    __tablename__ = "recipes"
+
+    id: Optional[int]=Field(default=None,primary_key=True)
+    title: str
+    ingredients:str
+    instructions:str
+    cooking_time: Optional[int]=Field(default=None)
+    created_at: datetime = Field(default_factory=lambda: datetime.now(timezone.utc).replace(tzinfo=None))
+
+    user_id: int = Field(foreign_key="users.id")
+    user: Optional[UserDB] = Relationship(back_populates="recipes")
+
 
 class UserCreate(BaseModel):
     username: str
@@ -11,3 +36,16 @@ class UserInDB(BaseModel):
 class Token(BaseModel):
     access_token:str
     token_type: str = "bearer"
+
+class RecipeCreate(BaseModel):
+    title:str
+    ingredients: str
+    instructions: str
+    cooking_time: Optional[int] = None
+
+class RecipeOut(BaseModel):
+    id: int
+    title:str
+    cooking_time: Optional[int]
+    created_at: datetime
+    user_id: int
