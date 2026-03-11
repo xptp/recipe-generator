e0f003cf91616eaa01d6b747a678dc87b7afbe9f
Ya <nesquik1723@gmail.com>
2026-03-11 10:07:03 +0300

    add authSlice login

--- frontend/src/store/slice/authSlice.ts
+++ frontend/src/store/slice/authSlice.ts
@@ -1,5 +1,5 @@
 import { createSlice,createAsyncThunk } from "@reduxjs/toolkit";
-import type  {UserInDB,RegisterData} from "../../types/auth"
+import type  {UserInDB,RegisterData, Token} from "../../types/auth"
 import authService from "../../service/authService";
 import axios from "axios";
 
@@ -12,18 +12,35 @@ interface AuthState {
     error: string | null;
 }
 
-export const signUp = createAsyncThunk<void, RegisterData, {rejectValue:string}>(
+export const signUp = createAsyncThunk<Token, RegisterData, {rejectValue:string}>(
     'auth/signUp',
     async (credentials, {rejectWithValue })=>{
         try {
-            await authService.register(credentials)
+           return await authService.register(credentials)
             
         } catch (e){
            if (axios.isAxiosError(e)){
             const errorMessage = e.response?.data || 'Ошибка регистрации'
             return rejectWithValue(errorMessage)
            }
+           return rejectWithValue("Непредвиденная ошибка регистрации, AuthSlice-signUp")
         }
+    }
+)
+
+export const signIn = createAsyncThunk<Token, RegisterData, {rejectValue:string}>(
+    'auth/signIn',
+    async (credentials, {rejectWithValue })=>{
+        try {
+            return await authService.login(credentials)
+        } catch (error) {
+            if (axios.isAxiosError(error)){
+            const errorMessage = error.response?.data || 'Ошибка авторизации'
+            return rejectWithValue(errorMessage)
+           }
+           return rejectWithValue("Непредвиденная ошибка авторицазции, AuthSlice-signIn")
+        } 
+        
     }
 )
 
@@ -56,10 +73,25 @@ const authSlice = createSlice({
             state.loading=true;
             state.error=null;
         })
-        .addCase(signUp.fulfilled, (state)=>{
+        .addCase(signUp.fulfilled, (state,action)=>{
             state.loading=false;
+            state.token=action.payload.acсess_token;
+            state.refreshToken=action.payload.refresh_token;
         })
         .addCase(signUp.rejected, (state, action)=>{
+            state.loading=false;
+            state.error= action.payload as string
+        })
+        .addCase(signIn.pending, state=>{
+            state.loading=true;
+            state.error=null;
+        })
+        .addCase(signIn.fulfilled, (state,action)=>{
+            state.loading=false;
+            state.token=action.payload.acсess_token;
+            state.refreshToken=action.payload.refresh_token;
+        })
+        .addCase(signIn.rejected, (state, action)=>{
             state.loading=false;
             state.error= action.payload as string
         })
